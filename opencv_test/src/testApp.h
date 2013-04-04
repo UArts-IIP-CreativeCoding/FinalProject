4e854f5c4ea3e2947fea8190ba2748fdf4580112
bklevence <bklevence@uarts.edu>
2013-04-04 12:55:32 -0400

    hai

---
+++ opencv_test/src/testApp.h
@@ -0,0 +1,36 @@
+#pragma mark once
+
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+
+class testApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+    ofVideoGrabber vidGrabber;
+    ofxCvColorImage colorImg;
+    
+    ofxCvGrayscaleImage grayImage; //converts color image
+    ofxCvGrayscaleImage grayBg; //stores image of background
+    ofxCvGrayscaleImage grayDiff; //compare them
+
+    ofxCvContourFinder contourFinder;
+    int threshold;
+    bool learnBackground;
+    
+    
+		
+};
