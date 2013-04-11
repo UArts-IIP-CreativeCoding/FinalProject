fb3e8769c95992571939b9c19e19f1403d213398
Andrew Cotter <cotter.andrew.p@gmail.com>
2013-04-10 23:16:40 -0400

    Added some OpenCV functionality to the project and doing some preliminary experimentation with it.

---
+++ mirrorMirror2/src/testApp.cpp
@@ -0,0 +1,197 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    string title = "Mirror";
+    string titleTwo = "//Mirror";
+    string start = "Press Space to start";
+    bool begin = false;
+    bool mirrorStart = false;
+    mirror.loadFont("MavenProLight-200.otf", 72);
+    mirror2.loadFont("MaveProLight-200.otf", 72);
+    press.loadFont("MavenProLight-200.otf", 48);
+    select.loadFont("MavenProLight-200.otf", 36);
+    names.loadFont("MavenProLight-200.otf", 24);
+    directions.loadFont("MavenProLight-200.otf", 14);
+    triHeight = 215;
+    
+    
+    
+    w = 320;
+    h = 240;
+    
+    movie.initGrabber(w, h, true);
+    
+    //reserve memory for cv images
+    rgb.allocate(w, h);
+    hsb.allocate(w, h);
+    hue.allocate(w, h);
+    sat.allocate(w, h);
+    bri.allocate(w, h);
+    filtered.allocate(w, h);
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+    movie.update();
+    
+    if (movie.isFrameNew()) {
+        
+        //copy webcam pixels to rgb image
+        rgb.setFromPixels(movie.getPixels(), w, h);
+        
+        //mirror horizontal
+        rgb.mirror(false, true);
+        
+        //duplicate rgb
+        hsb = rgb;
+        
+        //convert to hsb
+        hsb.convertRgbToHsv();
+        
+        //store the three channels as grayscale images
+        hsb.convertToGrayscalePlanarImages(hue, sat, bri);
+        
+        //filter image based on the hue value were looking for
+        for (int i=0; i<w*h; i++) {
+            filtered.getPixels()[i] = ofInRange(hue.getPixels()[i],findHue-5,findHue+5) ? 255 : 0;
+        }
+        filtered.flagImageChanged();
+        
+        //run the contour finder on the filtered image to find blobs with a certain hue
+        contours.findContours(filtered, 50, w*h/2, 1, false);
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    //title screen
+    if (begin == false) {
+        ofBackground(0);
+        mirror.drawString("Mirror//Mirror", 50, 100);
+        //mirror2.drawString("//Mirror", 100, 100);
+        press.drawString("Press Space to Start", 50, 175);
+        triHeight = 215;
+    }
+    
+    //choose your character
+    if (begin == true and mirrorStart == false) {
+        ofBackground(0);
+        //list of personas
+        select.drawString("Select a critic:", 50, 175);
+        names.drawString("Sassy Sarah", 50, 225);
+        names.drawString("Accepting Alice", 50, 275);
+        names.drawString("Popular Pat", 50, 325);
+        names.drawString("Encouraging Eric", 50, 375);
+        names.drawString("Mother-in-Law Mary", 50, 425);
+        names.drawString("Smart Alec", 50, 475);
+        names.drawString("Dirty Doug", 50, 525);
+        //cursor
+        ofTriangle(25, triHeight-10, 25, triHeight+10, 40, triHeight);
+        //instructions
+        directions.drawString("w=up  s=down  a=back d=select", 50, ofGetHeight()-50);
+    }
+    
+    //mirror
+    if (mirrorStart == true) {
+        ofSetColor(255,255,255);
+        
+        //draw all cv images
+        rgb.draw(0,0);
+        hsb.draw(640,0);
+        hue.draw(0,240);
+        sat.draw(320,240);
+        bri.draw(640,240);
+        filtered.draw(0,480);
+        contours.draw(0,480);
+        
+        ofSetColor(255, 0, 0);
+        ofFill();
+        
+        //draw red circles for found blobs
+        for (int i=0; i<contours.nBlobs; i++) {
+            ofCircle(contours.blobs[i].centroid.x, contours.blobs[i].centroid.y, 20);
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+    //press space to start and b to return
+    if (key == 32) {
+        begin = true;
+    }
+    if (key == 'a' and mirrorStart == false) {
+        begin = false;
+    }
+    
+    //move the selector up and down
+    if (key == 'w') {
+        if (triHeight > 215){
+            triHeight -= 50;
+        }
+    }
+    if (key == 's') {
+        if (triHeight < 515){
+            triHeight += 50;
+        }
+    }
+    
+    
+    //select persona
+    if (key == 'd' and begin == true) {
+        mirrorStart = true;
+    }
+    
+    //return to persona select //doesn't work yet
+    if (key == 'x') {
+        mirrorStart == false;
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+    
+    //calculate local mouse x,y in image
+    int mx = x % w;
+    int my = y % h;
+    
+    //get hue value on mouse position
+    findHue = hue.getPixels()[my*w+mx];
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){
+    
+}
---
+++ mirrorMirror2/src/testApp.h
@@ -0,0 +1,207 @@
+#pragma once
+
+#ifndef _TEST_APP
+#define _TEST_APP
+
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+
+class testApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed  (int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    //void mousePressed(int x, int y, int button);
+    
+    ofVideoGrabber movie;
+    
+    ofxCvColorImage rgb,hsb;
+    ofxCvGrayscaleImage hue,sat,bri,filtered;
+    ofxCvContourFinder contours;
+    
+    int w,h;
+    int findHue;
+    
+    
+    ofTrueTypeFont mirror;
+    ofTrueTypeFont mirror2;
+    ofTrueTypeFont press;
+    ofTrueTypeFont select;
+    ofTrueTypeFont names;
+    ofTrueTypeFont directions;
+    string title;
+    string titleTwo;
+    string start;
+    bool begin;
+    bool mirrorStart;
+    int triHeight;
+    
+    
+    //Sarah's Strings
+    string ssHair1;
+    string ssHair2;
+    string ssHair3;
+    string ssHair4;
+    
+    string ssEyes1;
+    string ssEyes2;
+    string ssEyes3;
+    string ssEyes4;
+    
+    string ssEars1;
+    string ssEars2;
+    string ssEars3;
+    string ssEars4;
+    
+    string ssNose1;
+    string ssNose2;
+    string ssNose3;
+    string ssNose4;
+    
+    
+    //Alice's Strings
+    string aaHair1;
+    string aaHair2;
+    string aaHair3;
+    string aaHair4;
+    
+    string aaEyes1;
+    string aaEyes2;
+    string aaEyes3;
+    string aaEyes4;
+    
+    string aaEars1;
+    string aaEars2;
+    string aaEars3;
+    string aaEars4;
+    
+    string aaNose1;
+    string aaNose2;
+    string aaNose3;
+    string aaNose4;
+    
+    
+    //Pat's Strings
+    string ppHair1;
+    string ppHair2;
+    string ppHair3;
+    string ppHair4;
+    
+    string ppEyes1;
+    string ppEyes2;
+    string ppEyes3;
+    string ppEyes4;
+    
+    string ppEars1;
+    string ppEars2;
+    string ppEars3;
+    string ppEars4;
+    
+    string ppNose1;
+    string ppNose2;
+    string ppNose3;
+    string ppNose4;
+    
+    
+    //Eric's Strings
+    string eeHair1;
+    string eeHair2;
+    string eeHair3;
+    string eeHair4;
+    
+    string eeEyes1;
+    string eeEyes2;
+    string eeEyes3;
+    string eeEyes4;
+    
+    string eeEars1;
+    string eeEars2;
+    string eeEars3;
+    string eeEars4;
+    
+    string eeNose1;
+    string eeNose2;
+    string eeNose3;
+    string eeNose4;
+    
+    
+    //Mary's Strings
+    string mmHair1;
+    string mmHair2;
+    string mmHair3;
+    string mmHair4;
+    
+    string mmEyes1;
+    string mmEyes2;
+    string mmEyes3;
+    string mmEyes4;
+    
+    string mmEars1;
+    string mmEars2;
+    string mmEars3;
+    string mmEars4;
+    
+    string mmNose1;
+    string mmNose2;
+    string mmNose3;
+    string mmNose4;
+    
+    
+    
+    //Alec's Strings
+    string saHair1;
+    string saHair2;
+    string saHair3;
+    string saHair4;
+    
+    string saEyes1;
+    string saEyes2;
+    string saEyes3;
+    string saEyes4;
+    
+    string saEars1;
+    string saEars2;
+    string saEars3;
+    string saEars4;
+    
+    string saNose1;
+    string saNose2;
+    string saNose3;
+    string saNose4;
+    
+    
+    //Doug's Strings
+    string ddHair1;
+    string ddHair2;
+    string ddHair3;
+    string ddHair4;
+    
+    string ddEyes1;
+    string ddEyes2;
+    string ddEyes3;
+    string ddEyes4;
+    
+    string ddEars1;
+    string ddEars2;
+    string ddEars3;
+    string ddEars4;
+    
+    string ddNose1;
+    string ddNose2;
+    string ddNose3;
+    string ddNose4;
+    
+};
+#endif
