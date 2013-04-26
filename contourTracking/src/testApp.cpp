bcdbe4c4c2771345697d51513efa6a5e6a22d5a4
bklevence <bklevence@uarts.edu>
2013-04-26 07:57:11 -0400

    hiiiii

---
+++ contourTracking/src/testApp.cpp
@@ -0,0 +1,99 @@
+#include "testApp.h"
+
+//learning blob stuffs from joshua noble 14-2
+
+
+//--------------------------------------------------------------
+void testApp::setup(){
+
+    learnBackground = false;
+    
+    vidGrabber.setVerbose(true);
+    vidGrabber.initGrabber(320, 240);
+    //allocating memory for image pixel data
+    
+    colorImg.allocate(320, 240);
+    grayImage.allocate(320, 240);
+    grayBg.allocate(320, 240);
+    grayDiff.allocate(320, 240);
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+    vidGrabber.grabFrame();
+    //do we have a new frame?
+    if (vidGrabber.isFrameNew()){
+		colorImg.setFromPixels(vidGrabber.getPixelsRef());
+        grayImage = colorImg; // convert our color image to a grayscale image
+        if (learnBackground == true) {
+            grayBg = grayImage; // update the background image
+            learnBackground = false;
+        }
+		grayDiff.absDiff(grayBg, grayImage);
+        grayDiff.threshold(60);
+		contourFinder.findContours(grayDiff, 5, (340*240)/4, 4, false, true);
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	ofSetHexColor(0xffffff);
+    colorImg.draw(0, 0, 320, 240);
+	grayDiff.draw(0, 240, 320, 240);
+    ofRect(320, 0, 320, 240);
+    contourFinder.draw(320, 0, 320, 240);
+	ofColor c(255, 255, 255);
+	for(int i = 0; i < contourFinder.nBlobs; i++) {
+		ofRectangle r  = contourFinder.blobs.at(i).boundingRect;
+		r.x += 320; r.y += 240;
+		c.setHsb(i * 64, 255, 255);
+		ofSetColor(c);
+		ofRect(r);
+	}
+    
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+    learnBackground = true;
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
